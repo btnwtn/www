d7518e1fb61c5c35705f2c13327770db0d54a1cf
Brandon Newton <btnwtn@gmail.com>
2018-04-22 11:26:31 -0400

    support markdown

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,46 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-export default () => (
+export default ({ data }) => (
   <div>
+    <h1>BRandons Kewl Website</h1>
     yo <Link to="/files">files</Link>
+    <h2>Blog Posts: {data.allMarkdownRemark.totalCount}</h2>
+    {data.allMarkdownRemark.edges.map(({ node }) => (
+      <section key={node.id}>
+        <h1>
+          <Link to={node.fields.slug}>
+            {node.frontmatter.title}{' '}
+            <span style={{ fontSize: '1rem', color: '#65767d' }}>
+              {node.frontmatter.date}
+            </span>
+          </Link>
+        </h1>
+        <Link to={node.fields.slug}>
+          <p>{node.excerpt}</p>
+        </Link>
+      </section>
+    ))}
   </div>
 )
+
+export const query = graphql`
+  query IndexQuery {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`
