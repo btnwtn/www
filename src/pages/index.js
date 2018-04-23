feab12f7a4f32af3c1f3a6da0ad90bc5e159eacd
Brandon Newton <btnwtn@gmail.com>
2018-04-22 21:09:44 -0400

    lil bio

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,37 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import profileImage from './profile.jpg'
+
 export default ({ data }) => (
   <div>
-    <h1>btnwtn</h1>
+    <h1 style={{ fontSize: '1rem', marginTop: '2em', marginBottom: '2em' }}>
+      btnwtn
+    </h1>
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        marginBottom: '3em',
+      }}
+    >
+      <div style={{ marginRight: '1.5em' }}>
+        <img
+          src={profileImage}
+          alt=""
+          style={{
+            display: 'block',
+            width: '75px',
+            height: '75px',
+            borderRadius: '50%',
+          }}
+        />
+      </div>
+      <p style={{ margin: 0 }}>
+        Brandon Newton is a <i>Software Engineer</i> specializing in performant
+        and responsive Frontend development. Currently located in Brooklyn, NY.
+      </p>
+    </div>
     {data.allMarkdownRemark.edges.map(({ node }) => (
       <section key={node.id}>
         <h1>
